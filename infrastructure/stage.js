d8c33789bd77da5cfa2f1e15612b090791cf4ed7
Joanna Turban <joanna.turban@schibsted.pl>
2016-01-26 12:44:45 +0100

    .

--- infrastructure/stage.js
+++ infrastructure/stage.js
@@ -6,7 +6,7 @@ var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
 var stageProps = {
     name: 'theonlyrealbookinventory-stage',
-    domains: [ `theonlyrealbookinventory-stage.herokuapp.com` ],
+    domains: [ 'theonlyrealbookinventory-stage.herokuapp.com' ],
     config_vars: {
         MONGOLAB_URI: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln',
         NODE_ENV: 'stage'
