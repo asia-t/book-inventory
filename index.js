0d66c5de7cf43c0474067e6aca71ad0ab722debd
Joanna Turban <joanna.turban@schibsted.pl>
2016-01-26 09:39:31 +0100

    base app

---
+++ index.js
@@ -0,0 +1,6 @@
+var mongoRepository = require('./stockRepository')();
+var app = require('./app')(mongoRepository);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
+});
