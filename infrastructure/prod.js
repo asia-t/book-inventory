bc9330f1b117392e740de8f5afbf86699d792ef4
Joanna Turban <joanna.turban@schibsted.pl>
2016-01-26 13:22:44 +0100

    .

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -6,7 +6,7 @@ var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
 var prodProps = {
     name: 'theonlyrealbookinventory',
-    domains: [ `theonlyrealbookinventory.herokuapp.com` ]
+    domains: [ 'theonlyrealbookinventory.herokuapp.com']
 };
 
 var config = _.merge({}, sharedConfig, prodProps);
